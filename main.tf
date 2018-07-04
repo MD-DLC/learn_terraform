d27da6708dd7faa5870fcb1427055a212f439143
Michael DeLaCruz <michaeldelacruz@Michaels-MBP.hsd1.va.comcast.net>
2018-07-04 16:08:20 -0400

    more updates

--- main.tf
+++ main.tf
@@ -7,12 +7,17 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
 user_data = <<-EOF
             #!/bin/bash
             echo "Hello World DLC is in the house." > index.html
             nohup busybox httpd -f -p 8080 &
             EOF
+  tags {
+    Name = "Terraform-example-DLC"
+  }
+}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -22,10 +27,5 @@ resource "aws_security_group" "instance" {
     to_port   = 8080
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-  tags {
-    Name = "Terraform-example-DLC"
   }
 }
