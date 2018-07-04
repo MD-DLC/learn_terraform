a97654c4b50abe38c28835257b8d33f2d322e3f8
Michael DeLaCruz <michaeldelacruz@Michaels-MBP.hsd1.va.comcast.net>
2018-07-04 15:44:59 -0400

    addind basic web server

--- main.tf
+++ main.tf
@@ -8,6 +8,23 @@ resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
 
+user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello World DLC is in the house." > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress{
+    from_port = 8080
+    to_port   = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
   tags {
     Name = "Terraform-example-DLC"
   }
